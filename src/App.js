e23f5793081fe8b2b81ac7d77632a104b3edc3a9
Nabil Mazwardi <a1833491@student.adelaide.edu.au>
2024-07-18 15:35:18 +0930

    Styling of the calculator done a bit

--- src/App.js
+++ src/App.js
@@ -10,33 +10,56 @@ function Number({ value , onNumberClick }) {
   )
 }
 
-function Grid() {
-  const [number, setNumber] = useState(0);
+function Grid({ onScreen }) {
 
   function handleNumberClick(i) {
-    setNumber(i)
+    onScreen(i)
   }
 
   return (
-    <>
-    <div className='board-row'>
-      <Number value="1" onNumberClick={ () =>  handleNumberClick(1)  } />
-      <Number value="2" onNumberClick={ () =>  handleNumberClick(2)  } />
-      <Number value="3" onNumberClick={ () =>  handleNumberClick(3)  } />
+    <div className='number-grid'>
+      <div className='board-row'>
+        <Number value="1" onNumberClick={ () =>  handleNumberClick(1)  } />
+        <Number value="2" onNumberClick={ () =>  handleNumberClick(2)  } />
+        <Number value="3" onNumberClick={ () =>  handleNumberClick(3)  } />
+      </div>
+      <div className='board-row'>
+        <Number value="4" onNumberClick={ () =>  handleNumberClick(4)  } />
+        <Number value="5" onNumberClick={ () =>  handleNumberClick(5)  } />
+        <Number value="6" onNumberClick={ () =>  handleNumberClick(6)  } />
+      </div>
+      <div className='board-row'>
+        <Number value="7" onNumberClick={ () =>  handleNumberClick(7)  } />
+        <Number value="8" onNumberClick={ () =>  handleNumberClick(8)  } />
+        <Number value="9" onNumberClick={ () =>  handleNumberClick(9)  } />
+      </div>
     </div>
-    <div className='board-row'>
-      <Number value="4" onNumberClick={ () =>  handleNumberClick(4)  } />
-      <Number value="5" onNumberClick={ () =>  handleNumberClick(5)  } />
-      <Number value="6" onNumberClick={ () =>  handleNumberClick(6)  } />
-    </div>
-    <div className='board-row'>
-      <Number value="7" onNumberClick={ () =>  handleNumberClick(7)  } />
-      <Number value="8" onNumberClick={ () =>  handleNumberClick(8)  } />
-      <Number value="9" onNumberClick={ () =>  handleNumberClick(9)  } />
-    </div>
-    <p> { number } </p>
-    </>
   )
 }
 
-export default Grid
+function Screen({ number }) {
+  return (
+    <div className='screen'>
+      <p> { number } </p>
+    </div>
+  )
+}
+
+function Calculator() {
+  const [number, setNumber] = useState(0);
+
+  function handleScreen(i) {
+    setNumber(i);
+  }
+
+  return (
+    <div className='container'>
+      <div className='calculator'>
+        <Screen number={number} />
+        <Grid onScreen={ handleScreen }/>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator
