309786c3219495a94f160d4694ab7dd1927e3d7c
Nabil Mazwardi <a1833491@student.adelaide.edu.au>
2024-07-18 12:20:59 +0930

    Number can be clicked and value registed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+function Number({ value , onNumberClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={ onNumberClick }>
+      { value }
+    </button>
+  )
 }
 
-export default App;
+function Grid() {
+  const [number, setNumber] = useState(0);
+
+  function handleNumberClick(i) {
+    setNumber(i)
+  }
+
+  return (
+    <>
+    <div>
+      <Number value="1" onNumberClick={ () =>  handleNumberClick(1)  } />
+      <Number value="2" onNumberClick={ () =>  handleNumberClick(2)  } />
+      <Number value="3" onNumberClick={ () =>  handleNumberClick(3)  } />
+    </div>
+    <div>
+      <Number value="4" onNumberClick={ () =>  handleNumberClick(4)  } />
+      <Number value="5" onNumberClick={ () =>  handleNumberClick(5)  } />
+      <Number value="6" onNumberClick={ () =>  handleNumberClick(6)  } />
+    </div>
+    <div>
+      <Number value="7" onNumberClick={ () =>  handleNumberClick(7)  } />
+      <Number value="8" onNumberClick={ () =>  handleNumberClick(8)  } />
+      <Number value="9" onNumberClick={ () =>  handleNumberClick(9)  } />
+    </div>
+    <p> { number } </p>
+    </>
+  )
+}
+
+export default Grid
