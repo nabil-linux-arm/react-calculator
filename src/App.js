b0d630f44cacad8409aa1cac4b7a6e998a1bb269
Nabil Mazwardi <a1833491@student.adelaide.edu.au>
2024-07-22 11:27:48 +0930

    Added operators to the calculator.

--- src/App.js
+++ src/App.js
@@ -12,36 +12,36 @@ function CalcButton({ value, onButtonClick }) {
 
 function Grid({ onScreen }) {
 
-  function handleNumberClick(i) {
+  function handleButtonClick(i) {
     onScreen(i)
   }
 
   return (
     <div className='button-grid'>
       <div className='board-row'>
-        <CalcButton value="=" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="+" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="-" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="*" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="/" onButtonClick={ () =>  handleNumberClick(1)  } />
+        <CalcButton value="=" onButtonClick={ () =>  handleButtonClick('equal')  } />
+        <CalcButton value="+" onButtonClick={ () =>  handleButtonClick('add')  } />
+        <CalcButton value="-" onButtonClick={ () =>  handleButtonClick('minus')  } />
+        <CalcButton value="*" onButtonClick={ () =>  handleButtonClick('multiply')  } />
+        <CalcButton value="/" onButtonClick={ () =>  handleButtonClick('divide')  } />
       </div>
       <div className='board-row'>
-        <CalcButton value="0" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="1" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="2" onButtonClick={ () =>  handleNumberClick(2)  } />
-        <CalcButton value="3" onButtonClick={ () =>  handleNumberClick(3)  } />
+        <CalcButton value="0" onButtonClick={ () =>  handleButtonClick(1)  } />
+        <CalcButton value="1" onButtonClick={ () =>  handleButtonClick(1)  } />
+        <CalcButton value="2" onButtonClick={ () =>  handleButtonClick(2)  } />
+        <CalcButton value="3" onButtonClick={ () =>  handleButtonClick(3)  } />
       </div>
       <div className='board-row'>
-        <CalcButton value="C" onButtonClick={ () =>  handleNumberClick('clear')  } />
-        <CalcButton value="4" onButtonClick={ () =>  handleNumberClick(4)  } />
-        <CalcButton value="5" onButtonClick={ () =>  handleNumberClick(5)  } />
-        <CalcButton value="6" onButtonClick={ () =>  handleNumberClick(6)  } />
+        <CalcButton value="C" onButtonClick={ () =>  handleButtonClick('clear')  } />
+        <CalcButton value="4" onButtonClick={ () =>  handleButtonClick(4)  } />
+        <CalcButton value="5" onButtonClick={ () =>  handleButtonClick(5)  } />
+        <CalcButton value="6" onButtonClick={ () =>  handleButtonClick(6)  } />
       </div>
       <div className='board-row'>
-        <CalcButton value="CA" onButtonClick={ () =>  handleNumberClick(1)  } />
-        <CalcButton value="7" onButtonClick={ () =>  handleNumberClick(7)  } />
-        <CalcButton value="8" onButtonClick={ () =>  handleNumberClick(8)  } />
-        <CalcButton value="9" onButtonClick={ () =>  handleNumberClick(9)  } />
+        <CalcButton value="CA" onButtonClick={ () =>  handleButtonClick('all_clear')  } />
+        <CalcButton value="7" onButtonClick={ () =>  handleButtonClick(7)  } />
+        <CalcButton value="8" onButtonClick={ () =>  handleButtonClick(8)  } />
+        <CalcButton value="9" onButtonClick={ () =>  handleButtonClick(9)  } />
       </div>
     </div>
   )
@@ -56,27 +56,62 @@ function Screen({ number }) {
 }
 
 function Calculator() {
-  const [number, setNumber] = useState(0);
+  const [operand1, setOperand1] = useState(0);
+  const [operand2, setOperand2] = useState(0);
+  const [operator, setOperator] = useState('');
 
   function handleScreen(i) {
     if (i === 'clear') {
-      setNumber(0);
+      setOperand1(0);
       return;
     }
-    let new_number = number*10 + i
-    if (new_number < Math.pow(10, 8)) {
-      setNumber(new_number);
+
+    if (i === 'all_clear') {
+      setOperand1(0);
+      setOperand2(0);
+      return;
     }
-  }
+
+    let new_number = operand1*10 + i
+
+    if (Number.isFinite(i)) {
+      if (new_number < Math.pow(10, 8)) {
+        setOperand1(new_number);
+      }
+    } else {
+      if (i === 'equal') {
+        let result = calculateExpression(operand1, operand2, operator);
+        setOperand1(result);
+      } else {
+        setOperand2(operand1);
+        setOperand1(0);
+        setOperator(i)
+      }
+    }
+ }
 
   return (
     <div className='container'>
       <div className='calculator'>
-        <Screen number={number} />
+        <Screen number={ operand1 } />
         <Grid onScreen={ handleScreen }/>
       </div>
     </div>
   );
+}
+
+function calculateExpression(op1, op2, operator) {
+  // console.log(op2 + " " + operator + " " + op1)
+  if (operator === 'add') {
+    return op2 + op1;
+  } else if (operator === 'minus') {
+    return op2 - op1;
+  } else if (operator === 'multiply') {
+    return op2 * op1;
+  } else {
+    return Math.floor(op2/op1);
+  }
+
 }
 
 export default Calculator
